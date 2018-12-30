1cb396aebda438620d0d71bb13afb8e26f1fc36e
Andrew Hoyt <andrew@chillhammer.com>
2018-12-29 21:15:06 -0500

    Initial commit

---
+++ GenerateProject.bat
@@ -0,0 +1,2 @@
+call Vendor\Binary\premake\premake5.exe vs2017
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,61 @@
+workspace "FedoraFrisbee"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "Vendor/Lib/GLFW"
+	
+project "FedoraFrisbee"
+	location "GameEngineOne"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("Temp/Binary/" .. outputdir .. "/%{prj.name}")
+	objdir ("Temp/Intermediates/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "fedpch.h"
+	pchsource "Source/fedpch.cpp"
+
+	files
+	{
+		"Source/**.h",
+		"Source/**.cpp"
+	}
+
+	includedirs
+	{
+		"Source",
+		"Vendor/Lib/spdlog/include",
+		"Vendor/Lib/GLFW/include",
+		"Vendor/Lib/GLEW/include",
+	}
+
+	links
+	{
+		"GLFW",
+		"Vendor/Lib/GLFW/lib/Release/x64/glew32s.lib"
+		"opengl32.lib"
+	}
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "FED_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FED_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "FED_DIST"
+		optimize "On"
