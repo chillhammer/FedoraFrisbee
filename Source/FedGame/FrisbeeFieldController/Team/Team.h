9f7ad4b46259a95cf74d2a4946e401f2c08247e6
Andrew Hoyt <andrew@chillhammer.com>
2019-02-16 12:27:56 -0500

    Team Agents List

--- Source/FedGame/FrisbeeFieldController/Team/Team.h
+++ Source/FedGame/FrisbeeFieldController/Team/Team.h
@@ -1,10 +1,14 @@
 #pragma once
+#include <vector>
 
 namespace Fed
 {
 	// Team controller. Overwatch AI that controls players
 	class Team
 	{
-
+	public:
+		Team();
+	private:
+		std::vector<class FedoraAgent*> m_Agents;
 	};
 }
