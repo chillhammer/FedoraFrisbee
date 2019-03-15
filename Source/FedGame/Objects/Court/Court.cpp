d095ce1419cb20a291e3ee5c853203754983b568
Andrew Hoyt <andrew@chillhammer.com>
2019-03-15 15:57:53 -0400

    Removed Court Comments

--- Source/FedGame/Objects/Court/Court.cpp
+++ Source/FedGame/Objects/Court/Court.cpp
@@ -10,14 +10,12 @@ namespace Fed
 	Court::Court() 
 		:	GameObject("Court")
 	{
-		//SetBoundingBox(Vector3(0, 0, 0.35f), Vector3(0.4, 0.25f, 0.4));
 	}
 
 	bool Court::IsColliding(const GameObject & other) const
 	{
 		for (int i = 0; i < NUM_BOUNDING_BOXES; ++i)
 		{
-			// if (m_BoundingBoxes[i].IsIntersecting(ObjectTransform, other.ObjectTransform, other.m_BoundingBox)
 			// TODO: Replace all bounding boxes with an array
 		}
 		return false;
