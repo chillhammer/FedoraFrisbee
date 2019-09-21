e1b79494e7f95f1a6bd255836a2b57b0dc11703b
Andrew Hoyt <andrew@chillhammer.com>
2019-09-20 22:30:46 -0400

    Revert "Revert "Enable Move To Assist Score Event Handling""
    This reverts commit b133e8b845885d4a6c62c3b241fe6aad5302d811.

--- Source/FedGame/Objects/Agents/Components/TeamStates/FedoraAgentInputAITeamStates.h
+++ Source/FedGame/Objects/Agents/Components/TeamStates/FedoraAgentInputAITeamStates.h
@@ -11,5 +11,6 @@ namespace Fed::AgentAITeamStates
 	STATE_CLASS_SINGLETON(FedoraAgentInputAI, Intercept, );
 	STATE_CLASS_SINGLETON(FedoraAgentInputAI, MoveToScore, );
 	STATE_CLASS_SINGLETON(FedoraAgentInputAI, MoveToSteal, );
+	STATE_CLASS_SINGLETON(FedoraAgentInputAI, MoveToAssistScore, );
 	STATE_CLASS_SINGLETON(FedoraAgentInputAI, GlobalMovement, );
 }
